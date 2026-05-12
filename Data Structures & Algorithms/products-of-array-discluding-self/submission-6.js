be020474a4ccf84e7041483b909a2771cd0b9dc1
Ishenko-Bogdan <152037479+Bogdahn-Ishenko@users.noreply.github.com>
2026-05-13 01:36:57 +0300

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length
+        const res = new Array(n)
+
+
+        let pos = 1
+        for (let i = 0; i < n; i++) {
+            res[i] = pos
+            pos *= nums[i]
+        
+        }
+        pos = 1
+        console.log(res)
+        for (let i = n-1; i >= 0; i--) {
+            res[i] *= pos
+            pos *= nums[i]
+        }
+
+
+ 
+        return res
+    }
+    
+}
