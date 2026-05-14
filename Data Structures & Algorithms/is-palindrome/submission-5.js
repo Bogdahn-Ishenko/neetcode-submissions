e8dd0ecbb3ee88dfd74e0e02932b124ec93ee780
Ishenko-Bogdan <152037479+Bogdahn-Ishenko@users.noreply.github.com>
2026-05-14 20:17:09 +0300

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+
+        console.log(typeof s)
+        const testPolindrom = (s.match(/[a-z0-9]/gi) ?? []).join("").toLowerCase();
+        let end = testPolindrom.length - 1;
+        let start = 0;
+
+        for (let i = 0; i < Math.ceil(testPolindrom.length / 2); i++) {
+            if (testPolindrom[start] !== testPolindrom[end]) return false
+            start += 1
+            end -= 1
+        }
+
+        return true
+    }
+}
